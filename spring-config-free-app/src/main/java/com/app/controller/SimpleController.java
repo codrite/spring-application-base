789c47ecb7fdda5fe2b0460397c8590dbe0f38fa
Arnabh Chakravarty <carn76@gmail.com>
2014-07-12 01:09:10 -0400

    Default Json View Supported

--- spring-config-free-app/src/main/java/com/app/controller/SimpleController.java
+++ spring-config-free-app/src/main/java/com/app/controller/SimpleController.java
@@ -3,9 +3,10 @@ package com.app.controller;
 import com.app.config.DatabaseConfig;
 import com.app.web.config.WebConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -23,15 +24,33 @@ public class SimpleController {
     @Autowired
     private WebConfig webConfig;
 
-    @RequestMapping(value = "/config", method = RequestMethod.GET)
-    public List<String> listAllConfig() {
-        return Arrays.asList(new String[] {
-                                            databaseConfig.getUsername(),
-                                            databaseConfig.getPassword(),
-                                            databaseConfig.getUrl(),
-                                            webConfig.getAppname()
-                                          }
-                            );
+    @RequestMapping(value = "/config",
+                    method = RequestMethod.GET,
+                    produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+    public
+    @ResponseBody
+    InternalUseOnly listAllConfig() {
+        return new InternalUseOnly(Arrays.asList(new String[]
+                        {
+                                databaseConfig.getUsername(),
+                                databaseConfig.getPassword(),
+                                databaseConfig.getUrl(),
+                                webConfig.getAppname()
+                        }
+        )
+        );
+    }
+
+    public static class InternalUseOnly {
+        private List<String> internal;
+
+        InternalUseOnly(List<String> internal) {
+            this.internal = internal;
+        }
+
+        public List<String> getInternal() {
+            return internal;
+        }
     }
 
 }
--- spring-config-free-app/src/main/java/com/app/web/config/WebConfig.java
+++ spring-config-free-app/src/main/java/com/app/web/config/WebConfig.java
@@ -1,33 +1,51 @@
 package com.app.web.config;
 
+import com.app.controller.SimpleController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.MediaType;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+import org.springframework.web.servlet.view.xml.MarshallingView;
 
-import java.util.Arrays;
+import javax.xml.bind.JAXBException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Dispatcher Servlet Configuration
  */
 
 @Configuration
+@EnableWebMvc
 @ImportResource("classpath:config/web-config.xml")
-@ComponentScan(value = {"com.app.controller","com.app.web.config"})
-public class WebConfig {
+@ComponentScan(value = {"com.app.controller", "com.app.web.config"})
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Value("${appname}")
     private String appname;
 
     @Bean
-    public ContentNegotiatingViewResolver getContentNegotiatingViewResolver() {
+    public ContentNegotiatingViewResolver getContentNegotiatingViewResolver() throws JAXBException {
         ContentNegotiatingViewResolver contentNegotiatingViewResolver = new ContentNegotiatingViewResolver();
-        View view = new MappingJackson2JsonView();
-        contentNegotiatingViewResolver.setDefaultViews(Arrays.asList(view));
+
+        List<View> supportedViews = new ArrayList<View>();
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        jaxb2Marshaller.setClassesToBeBound(SimpleController.InternalUseOnly.class);
+        MarshallingView marshallingView = new MarshallingView(jaxb2Marshaller);
+        supportedViews.add(marshallingView);
+        supportedViews.add(new MappingJackson2JsonView());
+
+        contentNegotiatingViewResolver.setDefaultViews(supportedViews);
+
         return contentNegotiatingViewResolver;
     }
 
@@ -35,4 +53,15 @@ public class WebConfig {
         return appname;
     }
 
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false)
+                .favorParameter(false)
+                .ignoreAcceptHeader(true)
+                .parameterName("mediaType")
+                .useJaf(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("json", MediaType.APPLICATION_JSON);
+    }
 }
